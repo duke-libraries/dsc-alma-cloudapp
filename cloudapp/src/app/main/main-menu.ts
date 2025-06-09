9d6bb5bfc6d33d85f1672f4f3f704b52c8f40658
Jeff Fleming <jeffrey.fleming@duke.edu>
2025-06-09 12:38:48 -0500

    Remove Integration logs

--- cloudapp/src/app/main/main-menu.ts
+++ cloudapp/src/app/main/main-menu.ts
@@ -1,11 +1,5 @@
 export const menu = [
     {
-      title: 'Integ Logs',
-      text:  'Logs for Alma Integrations',
-      icon: 'uxf-icon uxf-list',
-      link: 'integrationlogs'
-    },
-    {
       title: 'Item History',
       text:  'Item history from Aleph',
       icon: 'uxf-icon uxf-list',
